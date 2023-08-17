c29721c460c3612a3c82f1d5d75b6fc653fa465f
landy <2080279925@qq.com>
2023-08-17 13:02:41 +0800

    修改头部

--- components/header.vue
+++ components/header.vue
@@ -23,7 +23,7 @@ const state = reactive({
 	userInfo: '',
 	address: {
 		sheng: '',
-		city: '',
+		city: '广州',
 		area: ''
 	}
 })
@@ -38,44 +38,44 @@ onMounted(() => {
 	// const that = this
 	const plateForm = uni.getSystemInfoSync().uniPlatform;
 	if (plateForm === 'web') {
-		address.value.city = '广州市';
+		address.value.city = '广州';
 		return;
 	}
 
-	uni.getLocation({
-		type: 'wgs84',
-		geocode: true,
-		success: function (res) {
-			console.log('获取成功：');
+	// uni.getLocation({
+	// 	type: 'wgs84',
+	// 	geocode: true,
+	// 	success: function (res) {
+	// 		console.log('获取成功：');
 
-			// 创建地图坐标对象
-			var point = new plus.maps.Point(res.longitude, res.latitude);
+	// 		// 创建地图坐标对象
+	// 		var point = new plus.maps.Point(res.longitude, res.latitude);
 
-			//静态方法，反向地理编码
-			plus.maps.Map.reverseGeocode(
-				point,
-				{},
-				(event) => {
-					var mapAddress = event.address; // 转换后的地理位置
-					var point = event.coord; // 转换后的坐标信息
-					var coordType = event.coordType; // 转换后的坐标系类型
-					var reg = /.+?(省|市|自治区|自治州|县|区)/g;
-					var addressList = mapAddress.match(reg).toString().split(',');
-					//注意 因为存在直辖市， 当所在地区为普通省市时，addressList.length == 3，city = addressList[1];当所在地区为直辖市时addressList.length == 2，city = addressList[0];
-					let city = addressList.length == 3 ? addressList[1] : addressList[0];
-					console.log(addressList[1], 'addressList', addressList);
-					address.value.city = addressList[1] || '广州市';
-					address.value.sheng = addressList[0] || '广东省';
-				},
-				function (e) {
-					console.log('失败回调', e);
-				}
-			);
-		},
-		fail() {
-			console.log('获取失败：');
-		}
-	});
+	// 		//静态方法，反向地理编码
+	// 		plus.maps.Map.reverseGeocode(
+	// 			point,
+	// 			{},
+	// 			(event) => {
+	// 				var mapAddress = event.address; // 转换后的地理位置
+	// 				var point = event.coord; // 转换后的坐标信息
+	// 				var coordType = event.coordType; // 转换后的坐标系类型
+	// 				var reg = /.+?(省|市|自治区|自治州|县|区)/g;
+	// 				var addressList = mapAddress.match(reg).toString().split(',');
+	// 				//注意 因为存在直辖市， 当所在地区为普通省市时，addressList.length == 3，city = addressList[1];当所在地区为直辖市时addressList.length == 2，city = addressList[0];
+	// 				let city = addressList.length == 3 ? addressList[1] : addressList[0];
+	// 				console.log(addressList[1], 'addressList', addressList);
+	// 				address.value.city = addressList[1] || '广州市';
+	// 				address.value.sheng = addressList[0] || '广东省';
+	// 			},
+	// 			function (e) {
+	// 				console.log('失败回调', e);
+	// 			}
+	// 		);
+	// 	},
+	// 	fail() {
+	// 		console.log('获取失败：');
+	// 	}
+	// });
 })
 
 
